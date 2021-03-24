3a004d3bc97a9afaea9047465a456b297eda6408
kevded <kevded@hotmail.com>
2021-03-24 20:07:55 +0100

    test: add integration test consumer & producer

---
+++ src/test/java/com/example/reactivekafkaconsumerandproducer/ReactiveConsumerServiceIntegrationTest.java
@@ -0,0 +1,19 @@
+package com.example.reactivekafkaconsumerandproducer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.kafka.test.context.EmbeddedKafka;
+import org.springframework.test.annotation.DirtiesContext;
+
+@SpringBootTest
+@DirtiesContext(classMode = DirtiesContext.ClassMode.AFTER_EACH_TEST_METHOD)
+@EmbeddedKafka(topics = {"${FAKE_PRODUCER_DTO_TOPIC}", "${FAKE_CONSUMER_DTO_TOPIC}"})
+class ReactiveConsumerServiceIntegrationTest {
+
+
+	@Test
+	void reactiveConsumerService_receiveFakeConsumerDtoFromTopic_saveFakeConsumerDto() {
+		// TODO
+	}
+
+}
